4cc3527112902f01640a0cf420970947dfd0b5fa
James Leasure <james.leasure@tri-c.edu>
2019-01-23 15:19:20 -0500

    update dp.py

--- Examples/Week2/dp.py
+++ Examples/Week2/dp.py
@@ -1,9 +1,11 @@
 def drawPolygon(myTurtle,sideLength,numSides):
-
     turnAngle = 360 / numSides
-
     for i in range(numSides):
-
         myTurtle.forward(sideLength)
-
         myTurtle.right(turnAngle)
+        
+# example usage
+# from dp import *
+# import turtle
+# t = turtle.Turtle()
+# drawPolygon(t, 10, 10)
