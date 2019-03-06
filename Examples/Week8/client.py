72cf6ed8c165888cf4b251972ac34ee24d158b72
James Leasure <james.leasure@tri-c.edu>
2019-03-06 12:27:12 -0500

    updated client

--- Examples/Week8/client.py
+++ Examples/Week8/client.py
@@ -7,17 +7,19 @@ soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 192.168.1.103 - Mike
 # 192.168.1.102 - Dylan
 
-addresses = ['192.168.1.104', '192.168.1.103', '192.168.1.102', '192.168.1.105']
-names = ['1. Kelton', '2. Mike', '3. Dylan', '4. James']
+addresses = ['192.168.1.104', '192.168.1.103', '192.168.1.102', '192.168.1.105', '127.0.0.1']
+names = ['1. Kelton', '2. Mike', '3. Dylan', '4. James', '5. Localhost']
+#NOTE: change the last port to the port your server is running on
+ports = ['12345', '', '12346', '12347', '12347']
 
 while(True):
     sendTo = input(f"Who would you like to send to? {names}")
-    if (sendTo not in ['1', '2', '3', '4']):
+    if (sendTo not in ['1', '2', '3', '4', '5']):
         continue
     port = '12345'
 
     sendTo = int(sendTo) - 1
-    soc.connect((addresses[sendTo], 80))
+    soc.connect((addresses[sendTo], int(ports[sendTo])))
     
     clients_input = input("Message to send: ")  
     soc.send(clients_input.encode("utf8")) # we must encode the string to bytes  
